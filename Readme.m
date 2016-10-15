b0d2049ecd70b23a14763f3e912f93bb652c79c6
vishwesh96 <vishwesh1696@gmail.com>
2016-10-15 19:43:45 +0530

    added readme

---
+++ Readme.m
@@ -0,0 +1,17 @@
+Institute Mess Management System 
+				-- by D-Minions
+
+
+Instructions 
+-------------
+sudo apt-get update
+sudo apt-get install python-pip
+sudo pip install virtualenv
+git clone http://github.com/vishwesh96/mess-management_system
+cd mess-management_system
+source newenv/bin/activate
+sudo pip-install django --upgrade  
+
+Version
+-------
+Using django version 1.10.2
