2917011c7125df9b88a627d2c07dd3018f6b2263
B Srinivas Naik <bsrinivasnaik96@gmail.com>
2016-10-18 22:02:41 +0530

    initail srinives_settings

--- mess_management/mess_management/settings.py
+++ mess_management/mess_management/settings.py
@@ -73,13 +73,22 @@ WSGI_APPLICATION = 'mess_management.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER': 'bsrinivas',
+        'HOST': '192.168.1.103',
+        'PORT': '5640',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
