11685964573ba136ffa730611c80c9bb37f9bb0d
vishwesh96 <vishwesh1696@gmail.com>
2016-10-18 20:55:53 +0530

    updated readme

--- mess_management/mess_management/settings.py
+++ mess_management/mess_management/settings.py
@@ -73,12 +73,17 @@ WSGI_APPLICATION = 'mess_management.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME':  'postgres',                                    # name of the database
+        'USER': 'vishwesh',
+        'HOST': 'localhost',
+        'PORT': '5640',
     }
 }
+        
 
 
 # Password validation
@@ -118,3 +123,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
