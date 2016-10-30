f85f09bfc52d357dc17306fcf439bdde98d8c05f
gowtham <gowtham10896@gmail.com>
2016-10-30 16:09:26 +0530

    added pages after student login

--- mess_management/mainApp/views.py
+++ mess_management/mainApp/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def home(request):
+	if request.method == 'GET':
+	    
+	    return render(request, "home.html")			##lol
+
+	if request.method == 'POST':
+
+		return render(request,"profile.html")
--- mess_management/mess_management/urls.py
+++ mess_management/mess_management/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+import mainApp.views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^login/', include('login.urls')),
+    url(r'^home/', mainApp.views.home, name='home'),
 ]
