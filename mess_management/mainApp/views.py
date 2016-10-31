af555b7e5fcd5f262df327a69eac7ff79517151d
vishwesh96 <vishwesh1696@gmail.com>
2016-10-31 17:19:51 +0530

    completed profile

--- mess_management/mainApp/views.py
+++ mess_management/mainApp/views.py
@@ -22,42 +22,48 @@ def profile(request):
 	if not loggedIn:
 		return HttpResponseRedirect("/login/")
 
-	if request.method == 'GET':
-		if request.GET.type == "student" :
-			record = Student.objects.get(ldap=request.session['id'])
+	if request.method == 'GET':	
+		print request.GET.get('type')
+		if request.GET.get('type') == "student" :
+			record = Student.objects.filter(ldap=request.session['id'])
 			if record : 
 				isEmpty = False
+				edit = request.GET.get('edit',False)				
+				return render(request,"studentProfile.html",{"isEmpty": isEmpty,"record": record[0], "ldap": request.session['id'],  "edit":edit  })
 			else:
 				isEmpty = True
-			return render(request,"studentProfile.html",{"isEmpty": isEmpty,"record": record, "ldap": request.session['id']})
+				return render(request,"studentProfile.html",{"isEmpty": isEmpty, "ldap": request.session['id']})
 
-		elif request.GET.type == "mess" :
-			record = MessAuthority.objects.get(ID=request.session['id'])
+		elif request.GET.get('type') == "mess" :
+			record = MessAuthority.objects.filter(ID=request.session['id'])
 			if record : 
 				isEmpty = False
+				return render(request,"messAuthorityProfile.html",{"isEmpty": isEmpty,"record": record[0], "ID": request.session['id']})
 			else:
 				isEmpty = True
-			return render(request,"messAuthorityProfile.html",{"isEmpty": isEmpty,"record": record, "ID": request.session['id']})
+				return render(request,"messAuthorityProfile.html",{"isEmpty": isEmpty, "ID": request.session['id']})
 
 		else :
 			message = "wrong type (student or mess) "
 			return render(request,"error.html",{"message": message})
 
 	elif request.method == 'POST':
-		if request.POST.type == "student" :
-			record = Student.objects.get(ldap=request.session['id'])
+		if request.POST.get('type') == "student" :
+			record = Student.objects.filter(ldap=request.session['id'])
 			if record :
 				record.delete()
-			s = Student(rollNo = request.POST.rollNo, name = request.POST.name, ldap = request.POST.ldap, roomNo = request.POST.roomNo, phoneNo = request.POST.phoneNo)
+			s = Student(rollNo = request.POST.get('rollNo'), name = request.POST.get('name'), ldap = request.POST.get('ldap'), roomNo = request.POST.get('roomNo'), phoneNo = request.POST.get('phoneNo'))
 			s.save()
-
-		elif request.POST.type == "mess" :
-			record = MessAuthority.objects.get(ID=request.session['id'])
+			return HttpResponseRedirect("/profile/?type=student")
+		
+		elif request.POST.get('type') == "mess" :
+			record = MessAuthority.objects.filter(ID=request.session['id'])
 			if record :
 				record.delete()
-			h  =  Hostel.objects.get(ID=request.POST.hostelID)
-			m = MessAuthority(ID = request.session['id'], name = request.POST.name, hostel= h , phoneNo = request.POST.phoneNo)
+			h  =  Hostel.objects.get(ID=request.POST.get('hostelID'))
+			m = MessAuthority(ID = request.session['id'], name = request.POST.get('name'), hostel= h , phoneNo = request.POST.get('phoneNo'))
 			m.save()
+			return HttpResponseRedirect("/home/")
 
 
 # Function to display stats
